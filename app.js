3c2f11ce4b9d2bcffc7b6b1054c120912b8ae956
lucafdossantos-ai <lucafdossantos@gmail.com>
2026-01-08 00:27:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-alert("Nutri+ carregado! Em breve: cálculo basal, metas e refeições :)");
+function calcular() {
+  const sexo = document.getElementById("sexo").value;
+  const idade = Number(document.getElementById("idade").value);
+  const peso = Number(document.getElementById("peso").value);
+  const altura = Number(document.getElementById("altura").value);
+
+  let tmb;
+
+  if (sexo === "m") {
+    tmb = 10*peso + 6.25*altura - 5*idade + 5;
+  } else {
+    tmb = 10*peso + 6.25*altura - 5*idade - 161;
+  }
+
+  document.getElementById("resultado").innerText = Math.round(tmb);
+}
